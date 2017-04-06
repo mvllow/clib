15663277322570d60c36eb131dcc0e8e075bd155
Tate Kwiecinski <Tate Kwiecinski>
2017-04-06 09:15:54 -0500

    Rebase in vanilla JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,116 +1,77 @@
-$(document).ready(function () {
-    const {app, clipboard, remote, shell} = require('electron')
-    const storage = require('electron-json-storage')
-    const robot = require('robotjs')
-    const path = 'C:\\Riot Games\\League of Legends\\LeagueClient.exe'
+'use strict'
 
-    var window = remote.getCurrentWindow()
-    var password = ''
-    var flags = false
+const {app, clipboard, remote, screen, shell} = require('electron')
+const storage = require('electron-json-storage')
+const robot   = require('robotjs')
 
-    storage.getMany(['locked', 'password', 'flags'], (e, d) => {
-        if (e) throw e
+let {width, height} = screen.getPrimaryDisplay().workAreaSize
 
-        if (d.locked.obj === true) lock()
-        else unlock()
+var defaultAppPath = 'C:\\Riot Games\\League of Legends\\LeagueClient.exe'
+var appWindow = remote.getCurrentWindow()
+var password = ''
 
-        if (d.password.obj) $('#password').val(d.password.obj)
+// dom elements
+var appClose         = document.getElementById('app-close'),
+    appHide          = document.getElementById('app-hide'),
+    fPassword        = document.getElementById('f-password'),
+    fPasswordWrapper = document.getElementById('f-password-wrapper'),
+    bLaunch          = document.getElementById('b-launch'),
+    bLaunchAuto      = document.getElementById('b-launch-auto');
 
-        if (d.flags.obj === true) flags = true
-        else flags = false
+// event listeners
+if (appClose) appClose.addEventListener('click', onDestroy)
+if (appHide) appHide.addEventListener('click', onPause)
+if (bLaunch) bLaunch.addEventListener('click', function() { launch() })
+if (bLaunchAuto) bLaunchAuto.addEventListener('click', function() { launch(true) })
 
-        getFlags()
-    })
+storage.get('password', (e, d) => {
+    if (e) throw e
 
-    $('#reset').click((e) => {
-        storage.clear((e) => {
-            if (e) throw e
-        })
-
-        unlock()
-
-        $('#password').val('')
-        clipboard.clear()
-    })
-
-    $('#lock').click((e) => {
-        lock()
-    })
-
-    $('#flags').click((e) => {
-        if (flags) flags = false
-        else flags = true
-
-        getFlags()
-    })
-
-    $('#passwordToggle').click((e) => {
-        if ($('#password').attr('type') == 'password') {
-            $('#password').attr('type', 'text')
-            $('#passwordToggle').addClass('active')
-        } else {
-            $('#password').attr('type', 'password')
-            $('#passwordToggle').removeClass('active')
-        }
-    })
-
-    $('#launch').click((e) => {
-            var opened = shell.openItem(path)
-            if (!opened) alert('League of Legends cannot be found. Please install to the default location. Custom paths will be enabled in future updates.')
-
-            password = setConfig('password', $('#password').val())
-
-            clipboard.writeText(password)
-
-            if (flags) {
-                setTimeout(function() {
-                    robot.moveMouse(1400, 408)
-                    robot.mouseClick()
-                    robot.typeString(password)
-                    robot.keyTap('enter')
-                    clipboard.clear()
-                    window.close()
-                }, 10000);
-            } else {
-                setTimeout(function() {
-                    clipboard.clear()
-                    window.close()
-                }, 25000);
-            }
-    })
-
-    function setConfig(k, v) {
-        storage.set(k, { obj: v })
-
-        return v
+    if (d.obj) {
+        if (fPassword) fPassword.value = d.obj
+        if (fPasswordWrapper) fPasswordWrapper.classList.add('is-focused')
     }
+})
 
-    function getFlags() {
-        if (flags) {
-            setConfig('flags', true)
-            $('#flags').addClass('active')
-            $('#flags').text('Flags Enabled')
-        } else {
-            setConfig('flags', false)
-            $('#flags').removeClass('active')
-            $('#flags').text('Flags Disabled')
-        }
+function launch(auto = false) {
+    var opened = shell.openItem(defaultAppPath)
+
+    if (!opened) alert('LCU cannot be found. Please install to the default location. Custom paths will be enabled in future updates.')
+
+    password = setConfig('password', fPassword.value)
+
+    if (auto) {
+        appWindow.minimize()
+
+        setTimeout(function() {
+            robot.moveMouse(width/2, height/2)
+            robot.mouseClick()
+            robot.keyTap('tab')
+            robot.keyTap('tab')
+            robot.typeString(password)
+            appWindow.close()
+        }, 10000);
+    } else {
+        clipboard.writeText(password)
+        appWindow.hide()
+
+        setTimeout(function() {
+            clipboard.clear()
+            appWindow.close()
+        }, 25000);
     }
+}
 
-    function lock() {
-        setConfig('locked', true)
+function setConfig(k, v) {
+    storage.set(k, { obj: v })
 
-        $('#lock').addClass('active')
-        $('#password').prop('disabled', true)
-        $('#password').attr('type', 'password')
-        $('#passwordToggle').attr('disabled', 'true')
-        $('#passwordToggle').removeClass('active')
-    }
+    return v
+}
 
-    function unlock() {
-        $('#lock').removeClass('active')
-        $('#password').prop('disabled', false)
-        $('#password').attr('type', 'password')
-        $('#passwordToggle').removeClass('active')
-    }
-});
+function onPause() {
+    appWindow.minimize()
+}
+
+function onDestroy() {
+    appWindow.close()
+}
